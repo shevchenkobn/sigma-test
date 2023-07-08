676c6a7c62619dadc9012bc27bc3d8694c41eacd
Boǧdan Shevchenko <shevchenkobn@gmail.com>
2023-07-08 23:23:12 +0200

    Finish implementation

--- godzilla.py
+++ godzilla.py
@@ -27,8 +27,8 @@ class Cell:
 
 Coords = tuple[int, int]
 """Component order is (y, x)"""
-LineRange = tuple[Coords, Coords]
-"""Component order is ((y1, y2), (x1, x2))"""
+LineRange = list[list[int, int, int], list[int, int, int]]
+"""Component order is [[y, x1, x2], [x, y1, y2]]"""
 
 
 @dataclass
@@ -62,28 +62,31 @@ class Tokyo:
             line = get_next_line()
             row = []
             for x in range(width):
-                kind = CellKind('.' if line[x] == 'R' else line[x])
+                kind = CellKind('.' if line[x] != 'R' else line[x])
                 godz = GodzillaState.Untouched
-                if line[x] is 'G':
+                if line[x] == 'G':
                     gpos = (y, x)
                     godz = GodzillaState.Current
-                elif line[x] is 'M':
+                elif line[x] == 'M':
                     mpos.append(Mech((y, x), deque()))
                 cell = Cell(kind, godz)
                 row.append(cell)
             m.append(row)
-        return cls(m, width, height, gpos, mpos, ((gpos[0], gpos[0]), (gpos[1], gpos[1])))
+        return cls(m, width, height, gpos, mpos, [[gpos[0], gpos[1], gpos[1]], [gpos[1], gpos[0], gpos[0]]])
 
     def refresh_godz_next(self) -> typing.Union[Coords, None]:
         self.next_gpos = self.get_godz_next()
         return self.next_gpos
+
+    def is_pos_valid(self, y: int, x: int):
+        return 0 <= y < self.height and 0 <= x < self.width
 
     def get_godz_next(self) -> typing.Union[Coords, None]:
         y, x = self.gpos
         first_untouched = None
         for (oy, ox) in self.directions:
             ny, nx = y + oy, x + ox
-            if ny < 0 or nx < 0:
+            if not self.is_pos_valid(ny, nx):
                 continue
             cell = self.map[ny][nx]
             if cell.godz == GodzillaState.Untouched:
@@ -106,37 +109,41 @@ class Tokyo:
         self.gpos = self.next_gpos
         return self.gpos
 
-    def refresh_godz_flood(self, for_next=False):
-        gpos = self.next_gpos if for_next else self.gpos
-        if not gpos:
-            return self.gflood
+    def refresh_godz_flood(self, force=False):
+        # gpos = self.next_gpos if for_next else self.gpos
+        # if not gpos:
+        #     return self.gflood
+        yr, xr = self.gflood
 
-        y, x = gpos
-        by, bx = self.gpos
-        yr, xr = [gpos[0], gpos[0]], [gpos[1], gpos[1]]
-        if not for_next or y < by or x != bx:
+        y, x = self.gpos
+        by, bx = yr[0], xr[0]
+        yr[0], xr[0] = y, x
+        if force or y < by or x != bx:
             for d in range(y - 1, -1, -1):
-                if self.map[d][x].kind != CellKind.Residential:
-                    yr[0] = d
-        if not for_next or y > by or x != bx:
+                yr[1] = d
+                if self.map[d][x].kind is CellKind.Residential:
+                    break
+        if force or y > by or x != bx:
             for d in range(y + 1, self.height):
-                if self.map[d][x].kind != CellKind.Residential:
-                    yr[1] = d
-        if not for_next or x < bx or y != by:
+                yr[2] = d
+                if self.map[d][x].kind is CellKind.Residential:
+                    break
+        if force or x < bx or y != by:
             for d in range(x - 1, -1, -1):
-                if self.map[y][d].kind != CellKind.Residential:
-                    xr[0] = d
-        if not for_next or x > bx or y != by:
+                xr[1] = d
+                if self.map[y][d].kind is CellKind.Residential:
+                    break
+        if force or x > bx or y != by:
             for d in range(x + 1, self.width):
-                if self.map[y][d].kind != CellKind.Residential:
-                    xr[1] = d
+                xr[2] = d
+                if self.map[y][d].kind is CellKind.Residential:
+                    break
 
-        self.gflood = tuple(yr), tuple(xr)
         return self.gflood
 
     def is_in_gflood(self, p: Coords):
         gfy, gfx = self.gflood
-        return gfy[0] <= p[0] <= gfy[1] and gfx[0] <= p[1] <= gfx[1]
+        return gfy[0] == p[0] and gfy[1] <= p[1] <= gfy[2] or gfx[0] == p[1] and gfx[1] <= p[0] <= gfx[2]
 
     def index_of_mech_fire(self):
         for i in range(len(self.mechs)):
@@ -157,7 +164,7 @@ class Tokyo:
                 for oy, ox in self.directions:
                     y, x = c[0] + oy, c[1] + ox
                     nc = (y, x)
-                    if nc in steps or self.map[y][x].kind == CellKind.Residential:
+                    if not self.is_pos_valid(y, x) or nc in steps or self.map[y][x].kind == CellKind.Residential:
                         continue
                     steps[nc] = c
                     if self.is_in_gflood(nc):
@@ -174,8 +181,12 @@ class Tokyo:
             path = deque(maxlen=len(steps) - 1)
             curr = last
             while curr:
-                path.appendleft(curr)
-                curr = steps[curr]
+                prev = steps[curr]
+                if prev:
+                    path.appendleft(curr)
+                    curr = prev
+                else:
+                    break
             m.path = path
 
     def update_mechs_pos(self):
@@ -184,23 +195,31 @@ class Tokyo:
                 continue
             m.pos = m.path.popleft()
 
+    def _print_map(self):
+        char_map = [[c.kind.value for c in r] for r in self.map]
+        for m in self.mechs:
+            char_map[m.pos[0]][m.pos[1]] = 'M'
+        char_map[self.gpos[0]][self.gpos[1]] = 'G'
+        for r in char_map:
+            print(''.join(r))
+
     def simulate(self):
-        self.refresh_godz_flood()
+        self.refresh_godz_flood(force=True)
         self.refresh_godz_next()
 
         while True:
             new_pos = self.update_gpos()
 
             if new_pos:
-                if self.index_of_mech_fire() > 0:
+                if self.index_of_mech_fire() >= 0:
                     return self.destroyed_count
 
+                self.refresh_godz_flood()
                 self.refresh_godz_next()
-                self.refresh_godz_flood(for_next=True)
 
             self.refresh_mech_paths()
             self.update_mechs_pos()
-            if self.index_of_mech_fire() > 0:
+            if self.index_of_mech_fire() >= 0:
                 return self.destroyed_count
 
 
