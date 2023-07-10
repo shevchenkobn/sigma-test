20589e5b8361bcadf484c7ec418a69c0d6337d7e
Boǧdan Shevchenko <shevchenkobn@gmail.com>
2023-07-10 21:10:43 +0200

    [WIP] Godzilla.

--- godzilla.cpp
+++ godzilla.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <map>
+#include <stack>
+#include <vector>
+#include <tuple>
 #include <memory>
+#include <string>
 
 using namespace std;
 
@@ -25,15 +29,62 @@ CellKindNameMap* initCellKindName(CellKindNameMap* m) {
   return m;
 }
 
-enum class Godzilla : uint8_t {
+enum class GodzillaState : uint8_t {
   Untouched = 0,
   Current,
   Visited
 };
+
+struct Cell {
+  CellKind kind;
+  GodzillaState godz;
+
+  Cell(CellKind kind, GodzillaState godz) : kind(kind), godz(godz) {}
+};
+
+struct Coords {
+  int y;
+  int x;
+
+  Coords(int y, int x) : y(y), x(x) {}
+};
+
+struct Mech {
+  Coords pos;
+  stack<Coords> path;
+  Coords goal;
+
+  Mech(Coords pos) : pos(pos), path(stack<Coords>()), goal(pos) {}
+};
+
+class Tokyo {
+  public:
+    static const Coords directions[];
+
+    const vector<vector<Cell>>* map;
+    const int width;
+    const int height;
+
+    const Coords gpos;
+    const vector<Mech>* mechs;
+    const tuple<int, int, int> gflood[2];
+    const Coords nextGpos;
+    const int destroyedCount;
+
+    /**
+     * Requires delete.
+     */
+    Tokyo* decode(int width, int height, string (*getNextLine)()) {
+      map = new vector<vector<Cell>>();
+      
+
+      return nullptr;
+    }
+};
+const Coords Tokyo::directions[] = { Coords(-1, 0), Coords(0, 1), Coords(1, 0), Coords(0, -1) };
 
 int main() {
   CellKindNameMap CellKindName;
   initCellKindName(&CellKindName);
   cout << CellKindName[CellKind::Empty] << endl;
 }
-
