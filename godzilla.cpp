0e005327a9d5ec5fb38e8afb76757c03cb5486b0
Boǧdan Shevchenko <shevchenkobn@gmail.com>
2023-07-13 19:58:06 +0200

    Finish refactor.

--- godzilla.cpp
+++ godzilla.cpp
@@ -12,10 +12,6 @@
 #include <unordered_set>
 #endif
 
-#define EACH_I(array, i) int i = 0; i < sizeof(array) / sizeof(array[0]); i += 1
-
-
-using namespace std;
 
 enum class CellKind : uint8_t {
   Empty = 0,
@@ -23,44 +19,36 @@ enum class CellKind : uint8_t {
   Destroyed
 };
 
-// TODO: char readonly enum
-const char CellTypeEmpty = '.'; 
-const char CellTypeResidential = 'R';
-const char CellTypeDestroyed = 'X';
-const char CellTypeGodzilla = 'G';
-const char CellTypeMech = 'M';
+enum class CellType : const char {
+  Empty = '.',
+  Residential = 'R',
+  Destroyed = 'X',
+  Godzilla = 'G',
+  Mech = 'M'
+};
 
 // TODO: readonly bi-directional map (cannot use boost)
-using CellKindNameMap = unordered_map<CellKind, const char>;
+using CellKindNameMap = std::unordered_map<CellKind, CellType>;
 CellKindNameMap CellKindName;
 CellKindNameMap* initCellKindName(CellKindNameMap* m) {
-  m->insert({ CellKind::Empty, CellTypeEmpty });
-  m->insert({ CellKind::Residential, CellTypeResidential });
-  m->insert({ CellKind::Destroyed, CellTypeDestroyed });
+  m->insert({ CellKind::Empty, CellType::Empty });
+  m->insert({ CellKind::Residential, CellType::Residential });
+  m->insert({ CellKind::Destroyed, CellType::Destroyed });
   return m;
 }
 const CellKind getCellKindOrEmpty(const char ch) {
   for (auto p : CellKindName) {
-    if (p.second == ch) {
+    if (ch == (char)p.second) {
       return p.first;
     }
   }
   return CellKind::Empty;
 }
 
-enum class GodzillaState : uint8_t {
+enum class GodzillaStatus : uint8_t {
   Untouched = 0,
   Current,
   Visited
-};
-
-struct Cell {
-  CellKind kind;
-  GodzillaState godz;
-
-  Cell() : kind(CellKind::Empty), godz(GodzillaState::Untouched) {}
-  Cell(CellKind kind, GodzillaState godz) : kind(kind), godz(godz) {}
-  Cell(const Cell& other) : kind(other.kind), godz(other.godz) {}
 };
 
 const int CoordsMax = 1e6;
@@ -69,10 +57,10 @@ const int CoordsMax = 1e6;
 struct Coords {
   static const Coords none;
 
-  int y;
-  int x;
+  int y = INT_MIN;
+  int x = INT_MIN;
 
-  Coords() : y(INT_MIN), x(INT_MIN) {}
+  Coords() {}
   Coords(int y, int x) : y(y), x(x) {}
   Coords(const Coords& other) : y(other.y), x(other.x) {}
 
@@ -89,8 +77,8 @@ struct Coords {
   }
 
   #ifdef _DEBUG
-  operator string() const {
-    return "(" + to_string(y) + ", " + to_string(x) + ")";
+  operator std::string() const {
+    return "(" + std::to_string(y) + ", " + std::to_string(x) + ")";
   }
   #endif
 };
@@ -103,20 +91,41 @@ struct std::hash<Coords> {
   }
 };
 
+struct MechState {
+  static const MechState none;
+
+  int mechI = -1;
+  int turnN = -1;
+  Coords from = Coords::none;
+
+  MechState() {}
+  MechState(int mechI, int turnN, Coords from) : mechI(mechI), turnN(turnN), from(from) {}
+};
+const MechState MechState::none = MechState();
+
+struct Cell {
+  CellKind kind = CellKind::Empty;
+  GodzillaStatus godz = GodzillaStatus::Untouched;
+
+  Cell() {}
+  Cell(CellKind kind, GodzillaStatus godz) : kind(kind), godz(godz) {}
+  Cell(const Cell& other) : kind(other.kind), godz(other.godz) {}
+};
+
 struct Mech {
   Coords pos;
-  stack<Coords> path;
-  Coords goal;
+  std::stack<Coords> path = std::stack<Coords>();
+  Coords goal = Coords::none;
 
-  Mech(const Coords& pos) : pos(pos), path(stack<Coords>()), goal(pos) {}
+  Mech(const Coords& pos) : pos(pos), goal(pos) {}
   Mech(const Mech& other) : pos(other.pos), path(other.path), goal(other.goal) {}
 };
 
 struct Flood : Coords {
-  pair<int, int> xLimits;
-  pair<int, int> yLimits;
+  std::pair<int, int> xLimits;
+  std::pair<int, int> yLimits;
 
-  Flood(int y, pair<int, int> xLimits, int x, pair<int, int> yLimits) : Coords(y, x), xLimits(xLimits), yLimits(yLimits) {}
+  Flood(int y, std::pair<int, int> xLimits, int x, std::pair<int, int> yLimits) : Coords(y, x), xLimits(xLimits), yLimits(yLimits) {}
   Flood(const Flood& other) : Coords(other), xLimits(other.xLimits), yLimits(other.yLimits) {}
 };
 
@@ -125,12 +134,9 @@ struct Flood : Coords {
  */
 class Tokyo {
   public:
-    static const Coords directions[];
+    static const std::vector<Coords> directions;
 
-    /**
-     * Vector is not used because width & height are present.
-     */
-    Cell** map;
+    std::vector<std::vector<Cell>> map;
     const int width;
     const int height;
 
@@ -139,9 +145,9 @@ class Tokyo {
      * Readonly vector of mutable objects.
      * TODO: make readonly without [std::span](https://en.cppreference.com/w/cpp/container/span) or custom class.
      */
-    vector<Mech>* mechs;
+    std::vector<Mech> mechs;
     Flood gFlood;
-    Coords nextGPos;
+    Coords nextGPos = Coords::none;
     int destroyedCount = 0;
 
     /**
@@ -149,45 +155,44 @@ class Tokyo {
      * Input stream is not being closed in the function.
      * FIXME: return std::unique_ptr<Tokyo> without consequent `delete` requirement.
      */
-    static unique_ptr<Tokyo> decode(istream& input) {
+    static std::unique_ptr<Tokyo> decode(std::istream& input) {
       int width, height;
       input >> width >> height;
 
-      auto map = new Cell*[height];
+      auto map = std::vector<std::vector<Cell>>(height, std::vector<Cell>(width)); // TODO: test it
       Coords gPos = Coords::none;
-      auto mechs = new vector<Mech>();
+      auto mechs = std::vector<Mech>();
       for (int y = 0; y < height; y += 1) {
-        auto row = new Cell[width];
+        auto& row = map[y];
         for (int x = 0; x < width; x += 1) {
           char c;
           input >> c;
           auto kind = getCellKindOrEmpty(c);
-          auto godz = GodzillaState::Untouched;
-          switch (c) {
-            case CellTypeGodzilla: {
+          auto godz = GodzillaStatus::Untouched;
+          switch ((CellType)c) {
+            case CellType::Godzilla: {
               gPos = Coords(y, x);
-              godz = GodzillaState::Current;
+              godz = GodzillaStatus::Current;
               break;
             }
-            case CellTypeMech: {
-              mechs->push_back(Mech(Coords(y, x)));
+            case CellType::Mech: {
+              mechs.push_back(Mech(Coords(y, x)));
               break;
             }
           }
           auto cell = Cell(kind, godz);
           row[x] = cell;
         }
-        map[y] = row;
-        input.ignore(numeric_limits<streamsize>::max(), '\n');
+        input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
       }
-      return unique_ptr<Tokyo>(new Tokyo(map, width, height, gPos, mechs));
+      return std::unique_ptr<Tokyo>(new Tokyo(map, width, height, gPos, mechs));
     }
 
-    bool isPosValid(Coords p) {
+    bool isPosValid(Coords p) const {
       return isPosValid(p.y, p.x);
     }
 
-    bool isPosValid(int y, int x) {
+    bool isPosValid(int y, int x) const {
       return 0 <= y && y < height && 0 <= x && x < width;
     }
 
@@ -199,20 +204,39 @@ class Tokyo {
       return nextGPos;
     }
 
-    Coords getGodzNext();
+    Coords getGodzNext() {
+      auto firstUntouched = Coords::none;
+      Coords o = Coords::none;
+      for (const auto d : directions) {
+        auto p = gPos + d;
+        if (!isPosValid(p)) {
+          continue;
+        }
+        auto cell = map[p.y][p.x];
+        if (cell.godz == GodzillaStatus::Untouched) {
+          if (!firstUntouched) {
+            firstUntouched = p;
+          }
+          if (cell.kind == CellKind::Residential) {
+            return p;
+          }
+        }
+      }
+      return firstUntouched;
+    }
 
     Coords updateGPos() {
       if (!nextGPos) {
         return Coords::none;
       }
-      Cell& cell = map[gPos.y][gPos.x];
-      Cell& nextCell = map[nextGPos.y][nextGPos.x];
+      auto& cell = map[gPos.y][gPos.x];
+      auto& nextCell = map[nextGPos.y][nextGPos.x];
       if (nextCell.kind == CellKind::Residential) {
         nextCell.kind = CellKind::Destroyed;
         destroyedCount += 1;
       }
-      cell.godz = GodzillaState::Visited;
-      nextCell.godz = GodzillaState::Current;
+      cell.godz = GodzillaStatus::Visited;
+      nextCell.godz = GodzillaStatus::Current;
       gPos = nextGPos;
       return nextGPos;
     }
@@ -259,15 +283,15 @@ class Tokyo {
       return gFlood;
     }
 
-    bool isInGFlood(Coords p) {
-      auto f = gFlood; // WTF: f has garbage
+    bool isInGFlood(const Coords& p) const {
+      const auto& f = gFlood;
       return f.x == p.x && f.yLimits.first <= p.y && p.y <= f.yLimits.second
         || f.y == p.y && f.xLimits.first <= p.x && p.x <= f.xLimits.second;
     }
 
     int indexOfMechFire() {
-      for (int i = 0; i < mechs->size(); i += 1) {
-        auto m = mechs->at(i);
+      for (int i = 0; i < mechs.size(); i += 1) {
+        const auto& m = mechs[i];
         if (isInGFlood(m.pos)) {
           return i;
         }
@@ -275,10 +299,88 @@ class Tokyo {
       return -1;
     }
 
-    void refreshMechPaths();
+    void refreshMechPaths() {
+      for (auto& m : mechs) {
+        if (isInGFlood(m.goal)) {
+          continue;
+        }
+        std::queue<Coords> q({ m.pos });
+        auto steps = std::unordered_map<Coords, Coords>({ {m.pos, Coords::none} });
+
+        
+        Coords last = Coords::none;
+        while (!q.empty()) {
+          auto c = q.front();
+
+          for (const auto d : directions) {
+            auto nc = c + d;
+            if (!isPosValid(nc) || steps.find(nc) != steps.end() || map[nc.y][nc.x].kind == CellKind::Residential) {
+              continue;
+            }
+            steps[nc] = c;
+            if (isInGFlood(nc)) {
+              last = nc;
+              goto rmp1;
+            }
+            q.push(nc);
+          }
+          q.pop();
+        }
+        rmp1: std::queue<Coords>().swap(q); // clear the queue
+
+        if (!last || steps.size() == 1) {
+          m.path = std::stack<Coords>();
+          continue;
+        }
+        auto path = std::stack<Coords>();
+        auto curr = last;
+        while (curr) {
+          auto prev = steps[curr];
+          if (prev) {
+            path.push(curr);
+            curr = prev;
+          } else {
+            break;
+          }
+        }
+        m.path = path;
+        m.goal = last;
+      }
+    }
+
+    void refreshMechsState() {
+      std::queue<Coords> q;
+      auto steps = std::unordered_map<Coords, MechState>();
+      for (int i = 0; i < mechs.size(); i += 1) {
+        auto m = mechs[i];
+        q.push(m.pos);
+        steps[m.pos] = MechState(i, 0, Coords::none);
+      }
+
+      while (!q.empty()) {
+        auto c = q.front();
+
+        for (const auto d : directions) {
+          auto nc = c + d;
+          if (!isPosValid(nc)) {
+            continue;
+          }
+          auto prevStep = steps[c];
+          if (steps.find(nc) != steps.end()) {
+            steps[nc] = MechState(prevStep.mechI, prevStep.turnN + 1, c);
+          } else {
+            auto& step = steps[nc];
+            if (step.turnN < prevStep.turnN + 1) {
+
+            }
+          }
+          // steps[nc] = MechState(prevStep[i])
+        }
+      }
+    }
 
     void updateMechsPos() {
-      for (Mech& m : *mechs) {
+      for (auto& m : mechs) {
         if (m.path.empty()) {
           continue;
         }
@@ -289,22 +391,22 @@ class Tokyo {
 
     #ifdef _DEBUG
     void _printMap() {
-      unordered_set<Coords> mechsPos;
-      for (auto m : *mechs) {
+      std::unordered_set<Coords> mechsPos;
+      for (auto m : mechs) {
         mechsPos.insert(m.pos);
       }
       for (int y = 0; y < height; y += 1) {
         for (int x = 0; x < width; x += 1) {
           Coords p(y, x);
           if (p == gPos) {
-            cout << CellTypeGodzilla;
+            std::cout << (char)CellType::Godzilla;
           } else if (mechsPos.find(p) != mechsPos.end()) {
-            cout << CellTypeMech;
+            std::cout << (char)CellType::Mech;
           } else {
-            cout << CellKindName[map[y][x].kind];
+            std::cout << (char)CellKindName[map[y][x].kind];
           }
         }
-        cout << endl;
+        std::cout << std::endl;
       }
     }
     #endif
@@ -329,7 +431,7 @@ class Tokyo {
         updateMechsPos();
 
         #ifdef _DEBUG
-        cout << endl;
+        std::cout << std::endl;
         _printMap();
         #endif
 
@@ -340,111 +442,25 @@ class Tokyo {
       return destroyedCount;
     }
 
-    ~Tokyo() {
-      if (map && height > 0) {
-        for (int y = 0; y < height; y += 1) {
-          auto row = map[y];
-          if (row && width > 0) {
-            delete[] row;
-          }
-        }
-        delete[] map;
-      }
-      if (mechs) {
-        delete mechs;
-      }
-    }
-
   private:
-    Tokyo(Cell** map, int width, int height, Coords gPos, vector<Mech>* mechs)
+    Tokyo(std::vector<std::vector<Cell>> map, int width, int height, Coords gPos, std::vector<Mech> mechs)
       : map(map), width(width), height(height),
         gPos(gPos), mechs(mechs),
-        gFlood(gPos.y, { gPos.x, gPos.x }, gPos.x, { gPos.y, gPos.y }),
-        nextGPos(Coords::none), destroyedCount(0) {}
+        gFlood(gPos.y, { gPos.x, gPos.x }, gPos.x, { gPos.y, gPos.y }) {}
 };
-const Coords Tokyo::directions[] = { Coords(-1, 0), Coords(0, 1), Coords(1, 0), Coords(0, -1) };
-
-Coords Tokyo::getGodzNext() {
-  auto firstUntouched = Coords::none;
-  Coords o = Coords::none;
-  for (EACH_I(directions, i)) {
-    auto p = gPos + directions[i];
-    if (!isPosValid(p)) {
-      continue;
-    }
-    auto cell = map[p.y][p.x];
-    if (cell.godz == GodzillaState::Untouched) {
-      if (!firstUntouched) {
-        firstUntouched = p;
-      }
-      if (cell.kind == CellKind::Residential) {
-        return p;
-      }
-    }
-  }
-  return firstUntouched;
-}
-
-void Tokyo::refreshMechPaths() {
-  for (Mech& m : *mechs) {
-    if (isInGFlood(m.goal)) {
-      continue;
-    }
-    queue<Coords> q({ m.pos });
-    auto steps = unordered_map<Coords, Coords>({ {m.pos, Coords::none} });
-
-    
-    Coords last = Coords::none;
-    while (!q.empty()) {
-      auto c = q.front();
-
-      for (EACH_I(directions, i)) { // WTF: `steps` after this line gets additional element & the elements change on their own
-        auto nc = c + directions[i];
-        if (!isPosValid(nc) || steps.find(nc) != steps.end() || map[nc.y][nc.x].kind == CellKind::Residential) {
-          continue;
-        }
-        steps[nc] = c;
-        if (isInGFlood(nc)) {
-          last = nc;
-          goto rmp1;
-        }
-        q.push(nc);
-      }
-      q.pop();
-    }
-    rmp1: queue<Coords>().swap(q); // clear the queue
-
-    if (!last || steps.size() == 1) {
-      m.path = stack<Coords>();
-      continue;
-    }
-    auto path = stack<Coords>();
-    auto curr = last;
-    while (curr) {
-      auto prev = steps[curr];
-      if (prev) {
-        path.push(curr);
-        curr = prev;
-      } else {
-        break;
-      }
-    }
-    m.path = path;
-    m.goal = last;
-  }
-}
+const std::vector<Coords> Tokyo::directions = { Coords(-1, 0), Coords(0, 1), Coords(1, 0), Coords(0, -1) };
 
 int main() {
   initCellKindName(&CellKindName);
   
-  istream& ins = cin;
-  ostream& outs = cout;
+  auto& ins = std::cin;
+  auto& outs = std::cout;
 
   int t;
   ins >> t;
   for (int _ = 0; _ < t; _ += 1) {
     auto tokyo = Tokyo::decode(ins);
     int destroyedCount = tokyo->simulate();
-    outs << destroyedCount << endl;
+    outs << destroyedCount << std::endl;
   }
 }
